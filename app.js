278ed53037e32300c28cc875779e361e68f021a3
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-09-22 09:12:31 -0700

    update routing

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/interfaces/timereg/', indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
