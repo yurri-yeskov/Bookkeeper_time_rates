0d2029d4fc5eb4262ade5c8e26c42a564e79b864
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-09-24 05:37:42 -0700

    functionality with login info

--- controller/time_reporting.controller.js
+++ controller/time_reporting.controller.js
@@ -149,7 +149,7 @@ exports.findCustomerInfoWithYear = (req, res) => {
     let searchStr = req.body["search[value]"];
     searchStr = "WHERE "
     if(req.body["search[value]"])  {
-      if (acl_level != 1) searchStr = "bookkeeper_email = '" + my_email + "' AND ";
+      if (acl_level != 1) searchStr += "bookkeeper_email = '" + my_email + "' AND ";
       searchStr += ("(vv.january_spent::TEXT ILIKE '%" + searchStr + "%' OR vv.february_spent::TEXT ILIKE '%" + searchStr + 
                    "%' OR vv.march_spent::TEXT ILIKE '%" + searchStr + "%' OR vv.april_spent::TEXT ILIKE '%" + searchStr + 
                    "%' OR vv.may_spent::TEXT ILIKE '%" + searchStr + "%' OR vv.june_spent::TEXT ILIKE '%" + searchStr + 
@@ -179,7 +179,7 @@ exports.findCustomerInfoWithYear = (req, res) => {
                         "SELECT customer_id, primary_email, company_name, bookkeeper_name, bookkeeper_email, " +
                         "calc_timespent_month(customer_id, '" + service_from + "'::date, '" + service_until + "'::date) AS time_spent " +
                         "FROM temp_customer_time) AS aa) AS vv " + searchStr;
-    
+    console.log(query_search_count);
     // add offset and limit//
     let query_str = "SELECT vv.* FROM " +
                 "(SELECT COALESCE(time_spent[1], 0.00) as january_spent, COALESCE(time_spent[2], 0.00) as february_spent, " + 
