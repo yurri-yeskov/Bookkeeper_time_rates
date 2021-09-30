5d950e970b97a63a783af55ef9d5d26267846506
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-09-30 01:21:53 -0700

    add ocr_ui

--- public/javascripts/receipt-recog.js
+++ public/javascripts/receipt-recog.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
+    resize();
+});
+window.onresize = resize;
+
+function resize() {
     var h = $('#imagePanel').height() - 75;
     $('#previewImage').css('height', h + 'px');
     $('#resultViewer').css('height', h + 'px');
-});
-
-function resize() {
-    console.log("tyesfsdfasdfadsf");
 }
-  
-window.onresize = resize;
