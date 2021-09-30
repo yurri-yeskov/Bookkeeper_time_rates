19557466afef309e35f6e978d4f5083093c1231d
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-09-30 02:52:46 -0700

    add ocr_ui

--- public/javascripts/receipt-recog.js
+++ public/javascripts/receipt-recog.js
@@ -10,43 +10,24 @@ function resize() {
     $('#resultViewer').css('height', h + 'px');
 }
 
-// function loadImage() {
-//     $('#imagePath').bind('input keyup', function(){
-//       var $this = $(this);
-//       var delay = 500; // 0.5 seconds delay after last input
-//       var img_base_url = "https://login.ebogholderen.dk/lib/GetFile.php?filename="
+function loadImage() {
+    $('#imagePath').bind('input keyup', function(){
+      var $this = $(this);
+      var delay = 500; // 0.5 seconds delay after last input
+      var image_path = $('#imagePath').val();
+      var path_split = image_path.split('.');
   
-//       $('#previewImage').attr('src','./images/no-image.png');
-  
-//       clearTimeout($this.data('timer'));
-//       $this.data('timer', setTimeout(function(){
-//         $this.removeData('timer');
-        
-//         $('#previewImage').attr('src', img_base_url + $('#imagePath').val());
-//       }, delay));
-//     });
-// }
-
-
-document.getElementById('imagePath').onchange = function (evt) {
-    var tgt = evt.target || window.event.srcElement,
-        files = tgt.files;
-
-    // FileReader support
-    if (FileReader && files && files.length) {
-        var fr = new FileReader();
-        fr.onload = function () {
-            document.getElementById('previewImage').src = fr.result;
-        }
-        fr.readAsDataURL(files[0]);
-    }
-
-    // Not supported
-    else {
-        // fallback -- perhaps submit the input to an iframe and temporarily store
-        // them on the server until the user's session ends.
+      if (path_split[path_split.length - 1] != 'pdf' && path_split[path_split.length - 1] != 'PDF')
         $('#previewImage').attr('src','./images/no-image.png');
-    }
+      else 
+        $('#previewImage').attr('src','./images/pdf-file.png');
+  
+      clearTimeout($this.data('timer'));
+      $this.data('timer', setTimeout(function(){
+        $this.removeData('timer');
+        $('#previewImage').attr('src', image_path);
+      }, delay));
+    });
 }
 
 
