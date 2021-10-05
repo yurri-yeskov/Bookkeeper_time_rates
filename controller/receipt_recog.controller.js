18ec0b581631987272785a0194bde8eac1b5cd01
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-10-05 08:27:26 -0700

    Update receipt_recog.controller.js

--- controller/receipt_recog.controller.js
+++ controller/receipt_recog.controller.js
@@ -79,6 +79,9 @@ const ocrFunc = async (image_path, date_str, amount_str, word_str, res) => {
             let num_word_text = word_text.replace( /^\D+/g, '');
             let num_amount_str = amount_str.replace( /^\D+/g, '');
             console.log("BBBBBBBBBBBBBBBBBBBB", num_amount_str);
+            let testtester = "123.456,54()asdf   asdf";
+            testtester = testtester.replace( /^\D+/g, '');
+            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", testtester);
 
             let match_count = 0;
             for (let ii = 0; ii < num_word_text.length; ii++) {
