b5285cd4af4bdd9915f1e45d11b9bff58556465a
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-10-18 07:18:29 -0700

    Update receipt-recog.js

--- public/javascripts/receipt-recog.js
+++ public/javascripts/receipt-recog.js
@@ -150,7 +150,7 @@ function startRecognition() {
 
 function showDownloadPDFModal() {
     alert("Download PDF!!!");
-    var doc = new jsPDF('l', 'mm', 'a3')
+    var doc = new jsPDF('l', 'mm', 'a2')
 
     doc.setFontSize(18)
     doc.text('With content', 14, 22)
