0bc4ca611d1a8e47e05669b72f72ddc1a9790a16
Yurri Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-10-23 05:34:57 -0700

    change token-process

---
+++ controller/auth.controller.js
@@ -0,0 +1,18 @@
+const linkConfig = require("../config/links.config");
+const jwt = require("jsonwebtoken");
+
+exports.tokenVeryfy = (token) => {
+
+    const token_expiration = linkConfig.TOKEN_EXPIRATION;
+
+    const r_key = token.substring(0, 4);
+    const r_token = token.substring(4);
+    const decoded = jwt.verify(r_token, r_key);
+    
+    const now = new Date();
+    if (!decoded.timestamp) return false;
+    const token_timestamp = new Date(decoded.timestamp * 1000);
+    
+    if (now - token_timestamp > token_expiration) return false;
+    return decoded;
+};
--- controller/hourly.controller.js
+++ controller/hourly.controller.js
@@ -1,6 +1,6 @@
 const dbConfig = require("../config/db.config");
 const linkConfig = require("../config/links.config");
-const jwt = require("jsonwebtoken");
+const auth = require("../controller/auth.controller");
 const {Client} = require('pg');
 
 const client = new Client ({
@@ -31,85 +31,49 @@ exports.findAll = (req, res) => { // Select all bookkeeper info - id, name, hour
         res.redirect(linkConfig.OTHER_LINK);
         return;
     }
-
-    // const new_token = jwt.sign(
-    //     { user_id: "123", email: "email@mail.com" },
-    //     "123456",
-    //     {
-    //       expiresIn: "30s",
-    //     }
-    // );
-
-    // const new_decoded = jwt.verify(new_token, "123456");
-    // console.log(new_decoded);
-
-    // const token_expiration = linkConfig.TOKEN_EXPIRATION;
-    const token_expiration = 30000;
-
-    const t_key = req.body.user_token.substring(0, 4);
-    const t_token = req.body.user_token.substring(4);
-    const decoded = jwt.verify(t_token, t_key);
-    console.log("///////////////////////", decoded);
-    const now = new Date();
-    const token_timestamp = new Date(decoded.timestamp * 1000);
-    if (now - token_timestamp > token_expiration) {
+    const token_data = auth.tokenVeryfy(req.body.user_token);
+    if (!token_data) {
+        console.log("Token expired");
         res.redirect(linkConfig.OTHER_LINK);
         return;
     }
-    let pre_query_str = "SELECT user_email FROM interfaces.user_tokens WHERE user_token='" + req.body.user_token + "';";
 
-    client.query(pre_query_str, function(err, result) {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
-        let my_email = "";
+    const my_email = token_data.username;
+    let acl_level = admin_emails.includes(my_email) ? 1 : 0;
+    let acl_query_str = "SELECT interface_name FROM interfaces.acl WHERE user_email='" + my_email + "';";
+    let acl_array = [];
+    client.query(acl_query_str, function(err, result) {
         if (result.rows.length > 0) {
-            my_email = result.rows[0].user_email;
-        } else {
-            res.redirect(linkConfig.OTHER_LINK);
-            return;
-        }
-
-        let acl_level = admin_emails.includes(my_email) ? 1 : 0;
-        let acl_query_str = "SELECT interface_name FROM interfaces.acl WHERE user_email='" + my_email + "';";
-        let acl_array = [];
-        client.query(acl_query_str, function(err, result) {
-            if (result.rows.length > 0) {
-                for (let i=0; i<result.rows.length; i++) {
-                    acl_array[i] = result.rows[i].interface_name;
-                }
+            for (let i=0; i<result.rows.length; i++) {
+                acl_array[i] = result.rows[i].interface_name;
             }
-            let this_year = new Date();
-            this_year = this_year.getFullYear();
-    
-            let query_str = 
-                "SELECT task_manager.freelancers.id, worker_initials, price_per_hour, notes FROM task_manager.freelancers " + 
-                "LEFT JOIN task_manager.freelancer_roles ON task_manager.freelancers.worker_initials = task_manager.freelancer_roles.freelancer_short_name " + 
-                "WHERE task_manager.freelancer_roles.role_name = 'bookkeeper';"
-                
-            client.query(query_str, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    res.status(400).send(err);
-                }
-                res.render('index', {
-                    page:' Bookkeeper hourly rates', 
-                    this_year: this_year,
-                    other_link:linkConfig.OTHER_LINK, 
-                    menuId:'hourly-rates', 
-                    data:result.rows,
-                    my_email: my_email,
-                    acl_level: acl_level,
-                    acl_array: acl_array,
-                    user_token: req.body.user_token
-                });
+        }
+        let this_year = new Date();
+        this_year = this_year.getFullYear();
+
+        let query_str = 
+            "SELECT task_manager.freelancers.id, worker_initials, price_per_hour, notes FROM task_manager.freelancers " + 
+            "LEFT JOIN task_manager.freelancer_roles ON task_manager.freelancers.worker_initials = task_manager.freelancer_roles.freelancer_short_name " + 
+            "WHERE task_manager.freelancer_roles.role_name = 'bookkeeper';"
+            
+        client.query(query_str, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.render('index', {
+                page:' Bookkeeper hourly rates', 
+                this_year: this_year,
+                other_link:linkConfig.OTHER_LINK, 
+                menuId:'hourly-rates', 
+                data:result.rows,
+                my_email: my_email,
+                acl_level: acl_level,
+                acl_array: acl_array,
+                user_token: req.body.user_token
             });
-            return;
         });
         return;
-
-        
     });
 };
 
