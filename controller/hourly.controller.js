8268472657b004ae9896b2f9a07a1dadb7b56ff0
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-10-28 01:46:21 -0700

    test other link

--- controller/hourly.controller.js
+++ controller/hourly.controller.js
@@ -26,6 +26,9 @@ const admin_emails = auth.adminEmails();
 
 exports.findAll = (req, res) => { // Select all bookkeeper info - id, name, hourly, note
 
+    res.redirect("/");
+    return;
+
     if (!req.body.user_token) {
         console.log("Oops!");
         res.redirect(linkConfig.OTHER_LINK);
