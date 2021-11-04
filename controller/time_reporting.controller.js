62247123da1cdf9208d90d05a17941eeccfb50e3
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-11-04 01:25:49 -0700

    return origin

--- controller/time_reporting.controller.js
+++ controller/time_reporting.controller.js
@@ -169,109 +169,109 @@ exports.findCustomerInfoWithYear = (req, res) => {
   }
 
   const my_email = token_data.username;
-  let pre_query_str = "SELECT TRIM(CONCAT(first_name, ' ', last_name)) AS bookkeeper_name, COALESCE(price_per_hour, 0.00) as hourly FROM task_manager.freelancers WHERE email='" + my_email + "'";
-  console.log("-------------------ok");
-  client.query(pre_query_str, function(err, result) {
-    if (err) {
-      console.log(err);
-      res.status(400).send(err);  
-    }
-    console.log("asdfasdf----------------------");
-    let bookkeeper_full_name = "N/A"; 
-    let acl_level = admin_emails.includes(my_email) ? 1 : 0;
-    if (acl_level == 1) bookkeeper_full_name = "Admin";
-    else if (result.rows.length > 0) bookkeeper_full_name = result.rows[0].bookkeeper_name;
+  // let pre_query_str = "SELECT TRIM(CONCAT(first_name, ' ', last_name)) AS bookkeeper_name, COALESCE(price_per_hour, 0.00) as hourly FROM task_manager.freelancers WHERE email='" + my_email + "'";
+  // console.log("-------------------ok");
+  // client.query(pre_query_str, function(err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //     res.status(400).send(err);  
+  //   }
+  //   console.log("asdfasdf----------------------");
+  //   let bookkeeper_full_name = "N/A"; 
+  //   let acl_level = admin_emails.includes(my_email) ? 1 : 0;
+  //   if (acl_level == 1) bookkeeper_full_name = "Admin";
+  //   else if (result.rows.length > 0) bookkeeper_full_name = result.rows[0].bookkeeper_name;
 
-    let start_date = ""; let end_date = "";
-    if (req.body.sel_start_date)
-      start_date = moment(req.body.sel_start_date, 'DD-MM-YYYY').format('YYYY-MM-DD');
-    if (req.body.sel_end_date) 
-      end_date = moment(req.body.sel_end_date, 'DD-MM-YYYY').format('YYYY-MM-DD');
+  //   let start_date = ""; let end_date = "";
+  //   if (req.body.sel_start_date)
+  //     start_date = moment(req.body.sel_start_date, 'DD-MM-YYYY').format('YYYY-MM-DD');
+  //   if (req.body.sel_end_date) 
+  //     end_date = moment(req.body.sel_end_date, 'DD-MM-YYYY').format('YYYY-MM-DD');
     
-    let recordsTotal = 0;
-    let recordsFiltered = 0;
+  //   let recordsTotal = 0;
+  //   let recordsFiltered = 0;
 
-    let query_str = "SELECT (COALESCE(january_spent, 0.00) + COALESCE(february_spent, 0.00) + COALESCE(march_spent, 0.00) + " +
-                    "COALESCE(april_spent, 0.00) + COALESCE(may_spent, 0.00) + COALESCE(june_spent, 0.00) + " +
-                    "COALESCE(july_spent, 0.00) + COALESCE(august_spent, 0.00) + COALESCE(september_spent, 0.00) + " +
-                    "COALESCE(october_spent, 0.00) + COALESCE(november_spent, 0.00) + COALESCE(december_spent, 0.00)) as time_spent, reg_date, " +
-                    "customer_id, email_address, company_name, bookkeeper_name, task_type, period, delivery_year, note ";
-    let query_from = "FROM task_manager.time_entries WHERE deleted = false ";
+  //   let query_str = "SELECT (COALESCE(january_spent, 0.00) + COALESCE(february_spent, 0.00) + COALESCE(march_spent, 0.00) + " +
+  //                   "COALESCE(april_spent, 0.00) + COALESCE(may_spent, 0.00) + COALESCE(june_spent, 0.00) + " +
+  //                   "COALESCE(july_spent, 0.00) + COALESCE(august_spent, 0.00) + COALESCE(september_spent, 0.00) + " +
+  //                   "COALESCE(october_spent, 0.00) + COALESCE(november_spent, 0.00) + COALESCE(december_spent, 0.00)) as time_spent, reg_date, " +
+  //                   "customer_id, email_address, company_name, bookkeeper_name, task_type, period, delivery_year, note ";
+  //   let query_from = "FROM task_manager.time_entries WHERE deleted = false ";
     
-    if (bookkeeper_full_name != "Admin")
-      query_from = query_from + "AND bookkeeper_name = '" + bookkeeper_full_name + "' ";
-    if (start_date != "")
-      query_from = query_from + "AND reg_date >= '" + start_date + "'::date ";
-    if (end_date != "")
-      query_from = query_from + "AND reg_date <= '" + end_date + "'::date ";
-    // query_from = query_from + "ORDER BY reg_date ASC";
-    query_str = query_str + query_from;
+  //   if (bookkeeper_full_name != "Admin")
+  //     query_from = query_from + "AND bookkeeper_name = '" + bookkeeper_full_name + "' ";
+  //   if (start_date != "")
+  //     query_from = query_from + "AND reg_date >= '" + start_date + "'::date ";
+  //   if (end_date != "")
+  //     query_from = query_from + "AND reg_date <= '" + end_date + "'::date ";
+  //   // query_from = query_from + "ORDER BY reg_date ASC";
+  //   query_str = query_str + query_from;
 
-    let query_count = "SELECT COUNT(*) " + query_from;
+  //   let query_count = "SELECT COUNT(*) " + query_from;
 
-    let order_list = {
-      // 'january_spent': 1, 'february_spent': 2, 'march_spent': 3, 'april_spent': 4, 'may_spent': 5, 'june_spent': 6, 'july_spent': 7,
-      // 'august_spent': 8, 'september_spent': 9, 'october_spent': 10, 'november_spent': 11, 'december_spent': 12, 
-      // 'total_spent': 13, 'customer_id': 14, 'primary_email': 15, 'company_name': 16, 'bookkeeper_name': 17, 'time_spent': 19
-    };
+  //   let order_list = {
+  //     // 'january_spent': 1, 'february_spent': 2, 'march_spent': 3, 'april_spent': 4, 'may_spent': 5, 'june_spent': 6, 'july_spent': 7,
+  //     // 'august_spent': 8, 'september_spent': 9, 'october_spent': 10, 'november_spent': 11, 'december_spent': 12, 
+  //     // 'total_spent': 13, 'customer_id': 14, 'primary_email': 15, 'company_name': 16, 'bookkeeper_name': 17, 'time_spent': 19
+  //   };
 
-    let o_index = 'reg_date';
-    let o_dir = 'ASC';
-    if (req.body['order[0][column]']) {
-      o_index = 'columns[' + req.body['order[0][column]'] + '][data]';
-      o_index = req.body[o_index];
-      o_dir = req.body['order[0][dir]'];
-    }
-    // let order_by = " ORDER BY " + order_list[o_index] + " " + o_dir + " ";
-    let order_by = " ORDER BY " + o_index + " " + o_dir + " ";
-    let searchStr_r = req.body["search[value]"];
-    let searchStr = ""
-    if(req.body["search[value]"])  {
-      searchStr = "--------------- "
-      // let sub_searchStr = "(vv.january_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.february_spent::TEXT ILIKE '%" + searchStr_r + 
-      //                     "%' OR vv.march_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.april_spent::TEXT ILIKE '%" + searchStr_r + 
-      //                     "%' OR vv.may_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.june_spent::TEXT ILIKE '%" + searchStr_r + 
-      //                     "%' OR vv.july_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.august_spent::TEXT ILIKE '%" + searchStr_r + 
-      //                     "%' OR vv.september_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.october_spent::TEXT ILIKE '%" + searchStr_r +
-      //                     "%' OR vv.november_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.december_spent::TEXT ILIKE '%" + searchStr_r +
-      //                     "%' OR vv.total_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.customer_id::TEXT ILIKE '%" + searchStr_r + 
-      //                     "%' OR vv.primary_email::TEXT ILIKE '%" + searchStr_r + "%') ";
-      // searchStr += sub_searchStr;
-    }
-    let query_search_count = "SELECT COUNT(*) " + query_from + "AND " + searchStr;
+  //   let o_index = 'reg_date';
+  //   let o_dir = 'ASC';
+  //   if (req.body['order[0][column]']) {
+  //     o_index = 'columns[' + req.body['order[0][column]'] + '][data]';
+  //     o_index = req.body[o_index];
+  //     o_dir = req.body['order[0][dir]'];
+  //   }
+  //   // let order_by = " ORDER BY " + order_list[o_index] + " " + o_dir + " ";
+  //   let order_by = " ORDER BY " + o_index + " " + o_dir + " ";
+  //   let searchStr_r = req.body["search[value]"];
+  //   let searchStr = ""
+  //   if(req.body["search[value]"])  {
+  //     searchStr = "--------------- "
+  //     // let sub_searchStr = "(vv.january_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.february_spent::TEXT ILIKE '%" + searchStr_r + 
+  //     //                     "%' OR vv.march_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.april_spent::TEXT ILIKE '%" + searchStr_r + 
+  //     //                     "%' OR vv.may_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.june_spent::TEXT ILIKE '%" + searchStr_r + 
+  //     //                     "%' OR vv.july_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.august_spent::TEXT ILIKE '%" + searchStr_r + 
+  //     //                     "%' OR vv.september_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.october_spent::TEXT ILIKE '%" + searchStr_r +
+  //     //                     "%' OR vv.november_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.december_spent::TEXT ILIKE '%" + searchStr_r +
+  //     //                     "%' OR vv.total_spent::TEXT ILIKE '%" + searchStr_r + "%' OR vv.customer_id::TEXT ILIKE '%" + searchStr_r + 
+  //     //                     "%' OR vv.primary_email::TEXT ILIKE '%" + searchStr_r + "%') ";
+  //     // searchStr += sub_searchStr;
+  //   }
+  //   let query_search_count = "SELECT COUNT(*) " + query_from + "AND " + searchStr;
 
-    query_str = query_str + "AND " + searchStr + order_by;
-    if (req.body.length != -1)
-      query_str = query_str + " LIMIT " + req.body.length + " OFFSET " + req.body.start;
+  //   query_str = query_str + "AND " + searchStr + order_by;
+  //   if (req.body.length != -1)
+  //     query_str = query_str + " LIMIT " + req.body.length + " OFFSET " + req.body.start;
     
-    console.log("----------------query_str: ", query_str);
-    console.log("----------------query_count: ", query_count);
-    console.log("----------------query_search_count: ", query_search_count);
+  //   console.log("----------------query_str: ", query_str);
+  //   console.log("----------------query_count: ", query_count);
+  //   console.log("----------------query_search_count: ", query_search_count);
 
-    client.query(query_count, function(err, result) {
-      recordsTotal = result.rows[0].count;
-      client.query(query_search_count, function(err, result) {
-        recordsFiltered = result.rows[0].count;
-        client.query(query_str, function(err, result) {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          }
+  //   client.query(query_count, function(err, result) {
+  //     recordsTotal = result.rows[0].count;
+  //     client.query(query_search_count, function(err, result) {
+  //       recordsFiltered = result.rows[0].count;
+  //       client.query(query_str, function(err, result) {
+  //         if (err) {
+  //           console.log(err);
+  //           res.status(400).send(err);
+  //         }
 
-          var data = JSON.stringify({
-            "draw": req.body.draw,
-            "recordsFiltered": recordsFiltered,
-            "recordsTotal": recordsTotal,
-            "data": result.rows,
-            "this_year": req.body.this_year
-          });
-          console.log("--------------------", data);
-          res.send(data);
-        });
-      });
-    });
-  });
-  return;
+  //         var data = JSON.stringify({
+  //           "draw": req.body.draw,
+  //           "recordsFiltered": recordsFiltered,
+  //           "recordsTotal": recordsTotal,
+  //           "data": result.rows,
+  //           "this_year": req.body.this_year
+  //         });
+  //         console.log("--------------------", data);
+  //         res.send(data);
+  //       });
+  //     });
+  //   });
+  // });
+  // return;
 
   let service_from = req.body.this_year + "-01-01";
   let service_until = req.body.this_year + "-12-31";
