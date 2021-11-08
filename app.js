2a83b81644b249fde1b7c6d908507335716af904
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2021-11-08 06:59:55 -0800

    test base url

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.baseURL = "https://timebookkeeper.herokuapp.com"; // change baseurl;;
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
